b2bc8d3408c4fedba8937cac25cd9cc2c2ead762
Tim Malte GrC <Tim.Graefje@gmx.de>
2015-05-01 05:52:51 +0200

    Optimized by including lastUpdated statically

--- stats.js
+++ stats.js
@@ -1,6 +1,5 @@
 angular.module('statsApp', ['ngRoute'])
     .controller('statsController', ['$scope', '$location', function($scope,$location) {
-        $scope.last_updated_file = 'stats_files/lastUpdated.txt';
         $scope.isActive = function(viewLocation) {
             return viewLocation === $location.path();
         }
